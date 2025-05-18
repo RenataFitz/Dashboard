1fce47bdb451eeec3dca2797a27cc42082164eac
RenataFitz <sba22294@student.cct.ie>
2025-05-18 20:35:53 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -17,5 +17,5 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
+data = load_data(264505)
 data_load_state.text("Done! (using st.cache_data)")
