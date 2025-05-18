3b16f2fe765d999dfb9f33755c6b1885eec76902
RenataFitz <sba22294@student.cct.ie>
2025-05-18 20:33:20 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -4,4 +4,18 @@ import numpy as np
 
 st.title("Movie Dashboard")
 
+DATE_COLUMN='time_stamp'
 
+DATA_URL = ('https://github.com/CCT-Dublin/machine-learning-for-business-ca2-RenataFitz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
