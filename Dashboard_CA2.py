d6de263270f3caa9020dfa4890e4189721bad7ac
RenataFitz <sba22294@student.cct.ie>
2025-05-20 16:13:34 +0100

    update

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -1,21 +1,24 @@
-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 import re
 
-
-
-movies = pd.read_csv("movies.csv", encoding='ISO-8859-1')
-ratings = pd.read_csv("rating.csv", encoding='ISO-8859-1')
-tags = pd.read_csv("tags.csv", encoding='ISO-8859-1')
-merged = pd.merge(ratings, movies, on='movieId', how='left')
-full = pd.merge(merged, tags[['movieId', 'tag']], on='movieId', how='left')
-full['year'] = full['title'].apply(lambda x: int(re.findall(r'\((\d{4})\)', x)[0]) if re.search(r'\((\d{4})\)', x) else None)
-full['primary_genre'] = full['genres'].apply(lambda x: x.split('|')[0] if pd.notnull(x) else 'Unknown')
-full['datetime'] = pd.to_datetime(full['timestamp'], unit='s')
- 
 st.title("🎬 Online Retail Movie Insights Dashboard")
+
+@st.cache_data
+def load_data():
+    movies = pd.read_csv("movies.csv", encoding='ISO-8859-1')
+    ratings = pd.read_csv("rating.csv", encoding='ISO-8859-1')
+    tags = pd.read_csv("tags.csv", encoding='ISO-8859-1')
+    merged = pd.merge(ratings, movies, on='movieId', how='left')
+    full = pd.merge(merged, tags[['movieId', 'tag']], on='movieId', how='left')
+    full['year'] = full['title'].apply(lambda x: int(re.findall(r'\((\d{4})\)', x)[0]) if re.search(r'\((\d{4})\)', x) else None)
+    full['primary_genre'] = full['genres'].apply(lambda x: x.split('|')[0] if pd.notnull(x) else 'Unknown')
+    full['datetime'] = pd.to_datetime(full['timestamp'], unit='s')
+    return full
+
+data_load_state = st.text('Loading data...')
+data_load_state.text("Done! (using st.cache_data)")
 
 # Sidebar Filters
 selected_genre = st.sidebar.selectbox("Select Genre", sorted(full_df['primary_genre'].unique()))
