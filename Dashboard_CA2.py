5c3013c3ef30a0c15133e9fdec4fdce498d31b56
RenataFitz <sba22294@student.cct.ie>
2025-05-22 12:10:14 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas as pd
+import gdown
 
 @st.cache_data
 def load_data():
-    url = "https://drive.google.com/uc?id=1UuY3I30O-AHSIw_I5QNp8-_JLZxnbrIu"
-    return pd.read_csv(url)
+    file_id = "1UuY3I30O-AHSIw_I5QNp8-_JLZxnbrIu"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    output = "temp_data.csv"
+    
+    gdown.download(url, output, quiet=False)
+    return pd.read_csv(output)
 
 df = load_data()
-
+st.title("🎬 Online Retail Movie Analytics Dashboard")
+st.write(df.head())
 
