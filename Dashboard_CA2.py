959f17cbf6115669cd7c78852f3a20cc5d921660
RenataFitz <sba22294@student.cct.ie>
2025-05-22 11:48:17 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-
 @st.cache_data
 def load_data():
-    return pd.read_csv("dash_df.csv")  
+
+    dash_df = "https://drive.google.com/file/d/1Ca5CBnhUL3vhmhoqHfe3CAN62VG8n3wW/view?usp=drive_link"
+    return pd.read_csv(dash_df)  
 
 # Load the data
 dash_df = load_data()
