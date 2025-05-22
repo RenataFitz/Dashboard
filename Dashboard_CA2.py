fec2e006bda23690e5ea8434da3546f7484667bd
RenataFitz <sba22294@student.cct.ie>
2025-05-22 14:37:38 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -37,3 +37,16 @@ filtered_df = dash_df[
 ]
 
 st.title("🎬 Online Retail Movie Analytics Dashboard")
+
+# Section 1: Top 10 Genres
+st.subheader("FIGURE 1 | Top Ten Genres")
+genre_counts = filtered_df['primary_genre'].value_counts().head(10).reset_index()
+genre_counts.columns = ['primary_genre', 'count']
+fig1, ax1 = plt.subplots(figsize=(12, 5))
+sns.barplot(data=genre_counts, y='primary_genre', x='count', palette='deep', ax=ax1)
+ax1.set_title('FIGURE: 1 | Top Ten Genre', fontsize=16, fontweight="bold")
+ax1.set_xlabel('Count', fontsize=12, fontweight="bold")
+ax1.set_ylabel('Genre', fontsize=12, fontweight="bold")
+for bar in ax1.patches:
+    ax1.text(bar.get_width() + 1000, bar.get_y() + bar.get_height() / 2, f'{int(bar.get_width()):,}', va='center', fontsize=12)
+st.pyplot(fig1)
