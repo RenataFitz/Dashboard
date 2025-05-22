79954476da44e0d1da11c5832d1be99a20ec844b
RenataFitz <sba22294@student.cct.ie>
2025-05-22 11:36:40 +0100

    Update Dashboard_CA2.py

--- Dashboard_CA2.py
+++ Dashboard_CA2.py
@@ -3,23 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 @st.cache_data
-
 def load_data():
-    movies = pd.read_csv("movies.csv", encoding='ISO-8859-1')
-    ratings = pd.read_csv("rating.csv", encoding='ISO-8859-1')
-    tags = pd.read_csv("tags.csv", encoding='ISO-8859-1')
-    merged = pd.merge(ratings, movies, on='movieId', how='left')
-    full = pd.merge(merged, tags[['movieId', 'tag']], on='movieId', how='left')
-    full = full.dropna(subset=['tag'])
-    full['primary_genre'] = full['genres'].str.split('|').str[0]
-    full = full.drop(['timestamp', 'genres'], axis=1)
-    full['year'] = full['title'].str.extract(r'\((\d{4})\)').astype(int)
-    return full
+    return pd.read_csv("dash_df.csv")  
 
-full = load_data()
+# Load the data
+dash_df = load_data()
 
+# Streamlit Title
 st.title("🎬 Online Retail Movie Analytics Dashboard")
 
 
